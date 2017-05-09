b527f99e3ae55f1dddf871c27d4d20a846d91df3
fishg <fish@jidanke.com>
2017-05-09 21:15:43 +0800

    脚本提交

---
+++ Assets/Script/IMTest.cs
@@ -0,0 +1,137 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using YouMe;
+using YIMEngine;
+
+public class IMTest : MonoBehaviour {
+
+    public InputField userIDInput;
+    public Button loginButton;
+    public Button logoutButton;
+    public Text logText;
+    public Text recvMsgText;
+
+    public InputField textMsgInput;
+
+    IMClient IM;
+
+    // Use this for initialization
+    void Start () {
+    
+        IM = IMClient.Instance;
+        IM.Initialize("YOUME670584CA1F7BEF370EC7780417B89BFCC4ECBF78", "yYG7XY8BOVzPQed9T1/jlnWMhxKFmKZvWSFLxhBNe0nR4lbm5OUk3pTAevmxcBn1mXV9Z+gZ3B0Mv/MxZ4QIeDS4sDRRPzC+5OyjuUcSZdP8dLlnRV7bUUm29E2CrOUaALm9xQgK54biquqPuA0ZTszxHuEKI4nkyMtV9sNCNDMBAAE=",new Config(){ServerZone=ServerZone.China});
+    
+        userIDInput.text = Random.Range(10000,999999).ToString();
+    }
+
+    public void OnLoginClick(){
+        userIDInput.interactable = false;
+
+        // 注册接收新消息的方法
+        IM.SetReceiveMessageListener( OnReceiveMessage );
+        // 断线事件监听
+        IM.SetDisconnectListener((disconnectEvt)=>{
+            Debug.Log("断线了");
+        });
+        // 被踢下线事件监听
+        IM.SetKickOffListener((kickoffEvt)=>{
+            Debug.Log("被踢下线了");
+        });
+
+        //开始登陆
+        IM.Login(userIDInput.text,"",(evt)=>{
+            if(evt.Code == StatusCode.Success){
+                userIDInput.interactable = false; loginButton.interactable = false; logoutButton.interactable = true;
+                //进入聊天频道
+                IM.JoinChannel(new IMChannel("5678"),(channelEvt)=>{
+                    if(channelEvt.code == StatusCode.Success){
+                        Log2UI("进入频道:5678 成功");
+                    }else{
+                        Log2UI("进入频道:5678 失败");
+                    }
+                });
+                Log2UI("登陆成功");
+                Log2UI("开始进入频道:5678");
+            }else{
+                userIDInput.interactable = true; loginButton.interactable = true; logoutButton.interactable = false;
+                Log2UI("登陆失败，错误码："+evt.Code);
+            }
+        });
+        
+    }
+
+    public void OnLogoutClick(){
+        IM.Logout((evt)=>{
+            userIDInput.interactable = true; loginButton.interactable = true; logoutButton.interactable = false;
+            Log2UI("注销登陆");
+        });
+    }
+
+    public void OnSendTextMsgClick(){
+        var msg = IM.SendTextMessage(userIDInput.text,ChatType.PrivateChat,textMsgInput.text,(code,textMsgObj)=>{
+            if (code == StatusCode.Success)
+            {
+                Log2UI("发送：" + textMsgObj.content + " 成功");
+            }else{
+                Log2UI("发送：" + textMsgObj.content + " 失败");
+            }
+        });
+    }
+
+    public void OnStartRecordVoiceClick(){
+        Log2UI("启动录音");
+        var msg = IM.StartRecordAudio(userIDInput.text, ChatType.PrivateChat, "",false, (code, audioMsg) =>
+        {
+            if(audioMsg.sendStatus == SendStatus.Sending){
+                Log2UI("语音发送中");
+                Log2UI("开始播放自己发送的语音");
+                // audioMsg.Play((audiomsg)=>{
+                //     Log2UI("完成播放自己发送的语音");
+                // });
+            }else if(audioMsg.sendStatus == SendStatus.Sended){
+                Log2UI("语音发送成功");
+            }else if(audioMsg.sendStatus == SendStatus.Fail){
+                Log2UI("语音发送失败");
+            }else if (code != StatusCode.Success) { 
+                 Log2UI("启动录音失败");
+            }
+        });
+    }
+	
+    public void OnStopRecordAndSendClick(){
+
+        bool ret = IMClient.Instance.StopRecordAndSendAudio();
+        if(ret){
+            Log2UI("停止录音成功");
+        }else{
+            Log2UI("停止录音失败，不会发送");
+        }
+    }
+
+	void OnReceiveMessage(IMMessage msg){
+		if(msg.messageType == MessageBodyType.Voice){
+            var audioMsg = (AudioMessage)msg;
+            ShowMsg(audioMsg);
+            Log2UI("下载语音文件");
+            audioMsg.Download((code,audiMsgObj)=>{
+                Log2UI("播放接收到的语音消息");
+                audiMsgObj.Play( null );
+            });
+        }else if(msg.messageType == MessageBodyType.TXT){
+            ShowMsg(msg);
+        }
+	}
+
+	void ShowMsg(IMMessage msg){
+        if (msg.messageType == MessageBodyType.TXT)
+        {
+            recvMsgText.text = "收到文本："+((TextMessage)msg).content + "\n" + recvMsgText.text;
+        }else{
+            recvMsgText.text = "收到语音："+((AudioMessage)msg).recognizedText + "\n" + recvMsgText.text;
+        }
+    }
+
+    void Log2UI(string log){
+        logText.text = log + "\n" + logText.text;
+    }
+}
---
+++ Assets/Script/TalkTest.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using YouMe;
+using YIMEngine;
+
+public class TalkTest : MonoBehaviour {
+
+    public InputField userIDInput;
+    public InputField channelInput;
+    public Button joinButton;
+    public Button leaveButton;
+    public Button microphoneButton;
+    public Button speakerButton;
+    public Text microphoneText;
+    public Text speakerText;
+    public Text logText;
+
+    TalkClient talk;
+
+    // Use this for initialization
+    void Start () {
+        UpdateUninitUI();
+        talk = TalkClient.Instance;
+        Log2UI("开始初始化请求");
+        talk.Initialize("YOUME670584CA1F7BEF370EC7780417B89BFCC4ECBF78", "yYG7XY8BOVzPQed9T1/jlnWMhxKFmKZvWSFLxhBNe0nR4lbm5OUk3pTAevmxcBn1mXV9Z+gZ3B0Mv/MxZ4QIeDS4sDRRPzC+5OyjuUcSZdP8dLlnRV7bUUm29E2CrOUaALm9xQgK54biquqPuA0ZTszxHuEKI4nkyMtV9sNCNDMBAAE=",new TalkConfig(),(InitEvent evt)=>{
+            if(evt.Code == StatusCode.Success){
+                Log2UI("初始化成功");
+                UpdateInintedUI();
+            }else{
+                Log2UI("初始化失败，状态码："+evt.Code);
+            }
+        });
+    
+        userIDInput.text = Random.Range(10000,999999).ToString();
+        channelInput.text = "channel_888";
+    }
+
+    public void OnJoinChannelClick()
+    {
+        UpdateJoinChannelUI();
+        var channelID = channelInput.text;
+        //开始进语音频道
+        Log2UI("开始进入语音频道");
+
+        talk.SetUserID(userIDInput.text);
+        talk.JoinChannel(new TalkChannel(channelID), (ChannelEvent evt) => { 
+            if(evt.code == StatusCode.Success)
+            {
+                Log2UI("进入语音频道成功");
+            }else{
+                UpdateLeavedChannelUI();
+                Log2UI("进入语音频道失败，错误码："+evt.code);
+            } 
+       });
+    }
+
+    public void OnLeaveClick(){
+        Log2UI("发起离开语音频道请求");
+        leaveButton.interactable = false;
+        talk.LeaveAllChannel((ChannelEvent evt)=>{
+            UpdateLeavedChannelUI();
+            if(evt.code == StatusCode.Success)
+            {
+                Log2UI("成功离开语音频道");
+            }else{
+                Log2UI("不在语音频道，不需要调用离开频道");
+            } 
+        });
+    }
+
+    void Log2UI(string log){
+        logText.text = log + "\n" + logText.text;
+    }
+
+    public void SwitchMicrophone(){
+        if(talk.IsMicrophoneOpen()){
+            talk.CloseMicrophone();
+            microphoneText.text = "麦克风：关";
+        }else{
+            talk.OpenMicrophone();
+            microphoneText.text = "麦克风：开";
+        }
+    }
+
+    public void SwitchSpeaker(){
+        if(talk.IsSpeakerOpen()){
+            talk.CloseSpeaker();
+            speakerText.text = "扬声器：关";
+        }else{
+            talk.OpenSpeaker();
+            speakerText.text = "扬声器：开";
+        }
+    }
+
+    void UpdateJoinChannelUI(){
+        userIDInput.interactable = false;
+        channelInput.interactable = false;
+        joinButton.interactable = false;
+        leaveButton.interactable = true;
+        microphoneButton.interactable = true;
+        speakerButton.interactable = true;
+    }
+
+    void UpdateLeavedChannelUI(){
+        userIDInput.interactable = true;
+        channelInput.interactable = true;
+        joinButton.interactable = true;
+        leaveButton.interactable = false;
+        microphoneButton.interactable = false;
+        speakerButton.interactable = false;
+    }
+
+    void UpdateUninitUI(){
+        joinButton.interactable = false;
+        leaveButton.interactable = false;
+        microphoneButton.interactable = false;
+        speakerButton.interactable = false;
+    }
+
+    void UpdateInintedUI(){
+        joinButton.interactable = true;
+        leaveButton.interactable = false;
+    }
+}
